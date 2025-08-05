1f27c5d3fdbf6c811dcf08881296ca6dd78f6a23
Jogeshwaran <jokrish923@gmail.com>
2025-08-06 01:08:37 +0530

    dev tinder episode 4

--- src/app.js
+++ src/app.js
@@ -8,16 +8,37 @@ const app = express();
 
 //Use app.get() instead of app.use() for specific routes, and don’t use / with app.use() like that unless you’re using middleware.
 
-app.get("/",(req,res)=>{
-    res.send("testing request")
-})
 
-app.get("hello",(req,res)=>{
+
+app.use("/hello",(req,res)=>{
     res.send("Hellow hellow hellow")
 })
 
-app.get("test",(req,res)=>{
-    res.send("Hello from the server,otha")
+app.use("/test",(req,res)=>{
+    res.send("testing request")
+})
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the server,otha")
+// })
+
+//testing http methods
+app.get("/user",(req,res)=>{
+    res.send({firstName : "Jogeshwaran", lastName : 'S'})
+})
+
+app.post("/user", (req,res)=>{
+    //adding to the DB
+    res.send("changes saved to the database")
+})
+
+app.patch("/user", (req,res)=>{
+    //adding to the DB
+    res.send("changes updated to the database")
+})
+
+app.delete("/user", (req,res)=>{
+    res.send("changes deleted from database")
 })
 
 app.listen(7777,()=>{
