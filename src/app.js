a5def6416b6743ddb9aad06f055a0b38b3b1f031
Jogeshwaran <jokrish923@gmail.com>
2025-08-06 19:47:12 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -10,36 +10,73 @@ const app = express();
 
 
 
-app.use("/hello",(req,res)=>{
-    res.send("Hellow hellow hellow")
-})
-
-app.use("/test",(req,res)=>{
-    res.send("testing request")
-})
-
-// app.use("/",(req,res)=>{
-//     res.send("Hello from the server,otha")
+// app.use("/hello",(req,res)=>{
+//     res.send("Hellow hellow hellow")
 // })
 
-//testing http methods
-app.get("/user",(req,res)=>{
-    res.send({firstName : "Jogeshwaran", lastName : 'S'})
-})
+// app.use("/test",(req,res)=>{
+//     res.send("testing request")
+// })
 
-app.post("/user", (req,res)=>{
-    //adding to the DB
-    res.send("changes saved to the database")
-})
+// // app.use("/",(req,res)=>{
+// //     res.send("Hello from the server,otha")
+// // })
 
-app.patch("/user", (req,res)=>{
-    //adding to the DB
-    res.send("changes updated to the database")
-})
+// //testing http methods
+// app.get("/user",(req,res)=>{
+//     res.send({firstName : "Jogeshwaran", lastName : 'S'})
+// })
 
-app.delete("/user", (req,res)=>{
-    res.send("changes deleted from database")
-})
+// app.post("/user", (req,res)=>{
+//     //adding to the DB
+//     res.send("changes saved to the database")
+// })
+
+// app.patch("/user", (req,res)=>{
+//     //adding to the DB
+//     res.send("changes updated to the database")
+// })
+
+// app.delete("/user", (req,res)=>{
+//     res.send("changes deleted from database")
+// })
+
+//multiple router handlers
+
+app.use('/user',
+    
+(req,res,next)=>{
+    next()
+    console.log('1st response');
+    res.send('1st response')
+    
+},
+
+(req,res,next)=>{
+    console.log('2nd response');
+    res.send('2nd response')
+    next()
+},
+
+(req,res,next)=>{
+    console.log('3rd response');
+    res.send('3rd response')
+    next()
+},
+
+(req,res,next)=>{
+    console.log('4th response');
+    res.send('4th response')
+    next()
+},
+
+(req,res)=>{
+    console.log('5th response');
+    res.send('5th response') 
+    next()
+},
+
+)
 
 app.listen(7777,()=>{
     console.log(
